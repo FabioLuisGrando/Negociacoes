00b151798d804ba754420cccd855b4d3cda67a41
Fábio Luís Grando <fabioluisgrando@hotmail.com>
2021-10-25 16:15:44 -0300

    Commit em tudo pronto

---
+++ client/js/index.js
@@ -0,0 +1,36 @@
+var campos = [
+    document.querySelector("#data"),
+    document.querySelector("#quantidade"),
+    document.querySelector("#valor")
+]
+
+console.log(campos);
+
+var tbody = document.querySelector("table tbody");
+
+
+document.querySelector(".form").addEventListener("submit", function(){ //aqui ele pega o formulário inteiro ao invés de pegar só o botão
+
+    event.preventDefault();
+
+    var tr = document.createElement("tr");
+    
+    campos.forEach(function(campo){
+        var td = document.createElement("td");
+        td.textContent = campo.value; // o que vem entre as tags da TD vai ser o campo.value
+        tr.appendChild(td);
+    })
+
+    var tdVolume = document.createElement("td");
+    tdVolume.textContent = campos[1].value * campos[2].value;
+    tr.appendChild(tdVolume);
+
+    tbody.appendChild(tr);
+
+    campos[0].value = "";
+    campos[1].value = 1;
+    campos[2].value = 0;
+
+    campos[0].focus();
+
+});
